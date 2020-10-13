7072cf977de404860a41a46d393d777c7f638f3f
CallyCa <blackyrite@hotmail.com>
2020-10-13 16:18:40 -0300

    Add mongodb and mongoose

--- server.js
+++ server.js
@@ -1,5 +1,23 @@
+const mongoose = require("mongoose");
+const dotenv = require('dotenv');
+
 const app = require("./app");
-const port = 3000;
+const port = process.env.PORT || 3000;
+
+dotenv.config({path: './config.env'});
+
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+
+mongoose
+    .connect(DB,{ 
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+})
+    .then(() => console.log('DB connection successful'));
+
+//console.log(process.env)
 
 // Start Server
 app.listen(port, () => {
